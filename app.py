36713a1bced27d9293ace0ed26aa5215379a04f8
Emerson Ribeiro <97646023+emersonribeiroaraujo@users.noreply.github.com>
2024-09-20 21:46:17 -0300

    Meu primeiro commit

---
+++ app.py
@@ -0,0 +1,24 @@
+import streamlit as st
+import pandas as pd
+
+from sklearn.linear_model import LinearRegression
+
+df = pd.read_csv("pizzas.csv")
+
+modelo = LinearRegression()
+x = df[["diametro"]]
+y = df[["preco"]]
+modelo.fit(x, y)
+
+st.title("Prevendo o valor de uma pizza")
+st.divider()
+
+diametro = st.number_input("Digite o tamanho do diametro da pizza: ")
+
+if diametro:
+    preco_previsto = modelo.predict([[diametro]])[0][0]
+    st.write(f"o valor da pizza com diametro de {diametro:.2F} CM e de R${preco_previsto:.2F}.")
+    st.balloons()
+    
+
+
